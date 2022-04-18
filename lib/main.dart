3a9cc4f9507b2076a224eae1c83ab2cf3fdb5fdd
beinjamin <jauresbeinjamin@gmail.com>
2022-04-18 19:17:59 +0100

    Injection du state dans le main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_management__provider_blocs/pages/counter.statful.page.dart';
+import 'package:state_management__provider_blocs/provider/counter.state.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [],
+      providers: [ChangeNotifierProvider(create: (context) => CounterState())],
       child: MaterialApp(
         theme: ThemeData(primarySwatch: Colors.blue),
         routes: {"/": (context) => CounterPage()},
