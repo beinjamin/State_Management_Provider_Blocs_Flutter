0343dc414db0affc9b2d86a2b16a89d592e1d758
beinjamin <jauresbeinjamin@gmail.com>
2022-04-18 18:26:43 +0100

    Construction du projet et creation d'une simple route

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() => MyApp();
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/":(context)=>CounterPage();
+      }
+    );
+  }
+}
